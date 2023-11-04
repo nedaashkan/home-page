380cc58aef13bc8a0f8b69cb5a78f95d60c23c6a
nedaashkan <117584245+nedaashkan@users.noreply.github.com>
2023-11-04 23:42:36 +0330

    added css

--- home-page/src/Footer.jsx
+++ home-page/src/Footer.jsx
@@ -5,18 +5,18 @@ export default function Footer() {
   return (
     <div className="Footer">
       <div className="footer-box">
-        <div className="row gx-2 gx-md-2 gx-lg-0 mb-1 d-flex justify-content-center text-center">
-          <div className="col-4 col-md-4">
+        <div className="row gx-0 gx-md-0 gx-lg-0 mb-1 d-flex justify-content-center text-center">
+          <div className="col-4 col-md-4 col-lg-4">
             <span>
               تماس با ما
               <br />
               چطور خرید کنم؟
             </span>
           </div>
-          <div className="col-4 col-md-4">
+          <div className="col-4 col-md-4 col-lg-4">
             <h1>trendyol</h1>
           </div>
-          <div className="col-4 col-md-4">
+          <div className="col-4 col-md-4 col-lg-4">
             <span>
               درباره ما
               <br />
@@ -24,14 +24,14 @@ export default function Footer() {
             </span>
           </div>
         </div>
-        <div className="row gx-3 d-flex justify-content-center">
+        <div className="row gx-0 d-flex justify-content-center">
           <div className="col-1 col-md-1 col-lg-1 d-flex justify-content-center">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="25"
               height="25"
               fill="currentColor"
-              className="bi bi-telegram"
+              className="bi bi-telegram "
               viewBox="0 0 16 16"
             >
               <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.287 5.906c-.778.324-2.334.994-4.666 2.01-.378.15-.577.298-.595.442-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294.26.006.549-.1.868-.32 2.179-1.471 3.304-2.214 3.374-2.23.05-.012.12-.026.166.016.047.041.042.12.037.141-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8.154 8.154 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629.093.06.183.125.27.187.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.426 1.426 0 0 0-.013-.315.337.337 0 0 0-.114-.217.526.526 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09z" />
@@ -63,37 +63,37 @@ export default function Footer() {
           </div>
         </div>
         <div className="row mt-3 mt-lg-3 d-flex justify-content-center text-center">
-          <div className="col-3 col-lg-3">
+          <div className="col-4 col-md-3 col-lg-3">
             <span>
               Copyright © 2023 Eymur Inc.
               <br />
               All rights reserved.
             </span>
           </div>
-          <div className="col-6">
-            <div className="row gx-3 d-flex justify-content-center">
-              <div className="col-2">
+          <div className="col-5 col-md-5 col-lg-5">
+            <div className="row gx-3 gx-md-2 d-flex justify-content-center">
+              <div className="col-3 col-md-3 col-lg-3">
                 <img
                   src="./Footer/Rectangle 42.png"
                   alt="/"
                   className="w-100"
                 />
               </div>
-              <div className="col-2">
+              <div className="col-3 col-md-3 col-lg-3">
                 <img
                   src="./Footer/Rectangle 43.png"
                   alt="/"
                   className="w-100"
                 />
               </div>
-              <div className="col-2">
+              <div className="col-3 col-md-3 col-lg-3">
                 <img
                   src="./Footer/Rectangle 44.png"
                   alt="/"
                   className="w-100"
                 />
               </div>
-              <div className="col-2">
+              <div className="col-3 col-md-3 col-lg-3">
                 <img
                   src="./Footer/Rectangle 45.png"
                   alt="/"
@@ -102,7 +102,7 @@ export default function Footer() {
               </div>
             </div>
           </div>
-          <div className="col-3">
+          <div className="col-3 col-md-3 col-lg-3">
             <span>
               info@eymur.com
               <br />
