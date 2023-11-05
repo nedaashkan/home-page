bb4ceae182fa6aa2aa6973c7c8dd5a2816a82883
nedaashkan <117584245+nedaashkan@users.noreply.github.com>
2023-11-05 20:16:42 +0330

    added

--- home-page/src/ImageSection.jsx
+++ home-page/src/ImageSection.jsx
@@ -3,12 +3,14 @@ import "./ImageSection.css";
 
 export default function ImageSection() {
   return (
-    <div className="row d-flex justify-content-center gx-2 gx-md-2 gx-lg-1 mt-3 mt-md-4 mt-lg-5">
-      <div className="col-5 col-md-5 col-lg-4 d-flex justify-content-center">
-        <img src="/imageSection/D.png" alt="/" className="w-100" />
-      </div>
-      <div className="col-5 col-md-5 col-lg-4">
-        <img src="/imageSection/ol.png" alt="/" className="w-100" />
+    <div className="ImageSection">
+      <div className="row d-flex justify-content-center mt-3 mt-md-4 mt-lg-5">
+        <div className="col-5 col-md-5 col-lg-4 d-flex justify-content-center">
+          <img src="/imageSection/D.png" alt="/" className="w-100" />
+        </div>
+        <div className="col-5 col-md-5 col-lg-4">
+          <img src="/imageSection/ol.png" alt="/" className="w-100" />
+        </div>
       </div>
     </div>
   );
